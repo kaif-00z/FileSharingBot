8d394e54ed28a8eaa6d6818e3cda62f206a28f08
Kaif <88398455+kaif-00z@users.noreply.github.com>
2022-02-23 18:16:25 +0530

    better

--- bot/worker.py
+++ bot/worker.py
@@ -27,7 +27,7 @@ async def gen_link(event):
             "`Now send me Files/Messages one by one. After Sending All the Files/Message do `/done` .If you want to Cancel the Process do `/cancel` .`"
         )
         while True:
-            x = await cv.get_response()
+            x = await cv.get_response(timeout=60)
             if x.text.startswith("/cancel"):
                 return await x.reply("`Process Cancelled Successfully`")
             elif x.text.startswith("/done"):
